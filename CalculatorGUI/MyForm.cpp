fe061ce8be563d0420fe772f954e8f2b572e1793
Roman <Roman@DESKTOP-MHLUTND>
2025-05-01 22:47:39 +0400

    Start

--- CalculatorGUI/MyForm.cpp
+++ CalculatorGUI/MyForm.cpp
@@ -1,2 +1,12 @@
 #include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
 
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	CalculatorGUI::MyForm form;
+	Application::Run(% form);
+
+}
